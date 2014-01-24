a889d98fb48a24ef091cdcb36be3110e0c8d6bae
Volodymyr Medvid <vmedvid@riseup.net>
2014-01-24 23:53:00 +0200

    Add force option to signing script

--- sign.sh
+++ sign.sh
@@ -1,9 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+force=0
+
+for opt do
+  optarg=`expr "x$opt" : 'x[^=]*=\(.*\)'`
+  case "$opt" in
+  -f|--force)
+    force=1
+    ;;
+  --no-force)
+    force=0
+    ;;
+  esac
+done
 
 dir=`dirname $0`
 
 for pkg in `ls $dir/http/{i686,x86_64}/*.xz`; do
-  if [ ! -e $pkg.sig ]; then
+  if [[ $force -eq 1 ]]; then
+    rm -f $pkg.sig
+  fi
+  if [[ ! -e $pkg.sig ]]; then
     gpg --detach-sign --use-agent $pkg
   fi
 done
