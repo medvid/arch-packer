97bb28576f98fafcf18d7bdd35846053f77ac9ae
Volodymyr Medvid <vmedvid@riseup.net>
2014-01-24 22:19:25 +0200

    Update database script

--- db.sh
+++ db.sh
@@ -9,13 +9,28 @@ get_db() {
   local arch=$2
   local dir=$http_root/$arch
 
+  echo "Update $arch/$repo"
   mkdir -p $dir
-  wget $mirror/$repo/os/$arch/$repo.db -O $dir/$repo.db
+  wget $mirror/$repo/os/$arch/$repo.db -O $dir/$repo.db --quiet
 }
 
-for repo in core extra community; do
-  for arch in i686 x86_64; do
+# update repository databases
+for arch in i686 x86_64; do
+  for repo in core extra community; do
     get_db $repo $arch
   done
 done
 get_db multilib x86_64
+
+# update symlinks to local databases
+for arch in i686 x86_64; do
+  dir=$http_root/$arch
+
+  if [ ! -e $dir/local.db ]; then
+    if [ -e $dir/local.db.tar.gz ]; then
+      ln -s local.db.tar.gz $dir/local.db
+    else
+      echo "Warning: database $dir/local.db.tar.gz does not exist!"
+    fi
+  fi
+done
