de8e721495324c180c2a87fac47cbbcdb5cb185a
Volodymyr Medvid <vmedvid@riseup.net>
2014-01-27 16:42:34 +0200

    Fix multilib provisioner

--- scripts/multilib.sh
+++ scripts/multilib.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 
+# Uncomment multilib repository in pacman.conf
 sudo awk -i inplace '
 /^#\[multilib\]/{s=1}
 s>0{sub("^#","");s++}
@@ -8,5 +9,13 @@ s==3{s=0}
 {print}
 ' /etc/pacman.conf
 
-# --noconfirm doesn't work here
-yes | sudo pacman -Sy --noconfirm multilib-devel
+# Install multilib-devel group, replacing packages from base-devel
+# --noconfirm does not work here
+# 6 confirmations should be done:
+#   - select all packages in group (RET)
+#   - binutils (y)
+#   - gcc-libs (y)
+#   - gcc (y)
+#   - libtool (y)
+#   - pacman summary (y)
+echo -ne '\ny\ny\ny\ny\ny\n' | sudo pacman -Sy multilib-devel
